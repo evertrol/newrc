8aa337dbad846f1a634726445d298d516ac1f92f
Evert Rol <evert.rol@gmail.com>
2016-08-18 16:25:02 +1000

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+
+setup(
+    name="newrc",
+    version='0.1',
+    description=("Python netrc alternative with some extended capability"),
+    author="Evert Rol",
+    author_email="evert.rol@monash.edu",
+    url="https://github.com/evertrol/newrc",
+    license="PSF",
+    py_modules=["newrc"],
+)
